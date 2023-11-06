3ee3f9455306857043b970dedc0801d5cb933bf6
Darkrider123 <cosmin.ariton@gmail.com>
2023-11-06 14:05:24 +0200

    added restart function

--- main.py
+++ main.py
@@ -7,23 +7,28 @@ from sensible_config import HOST, PORT
 class HomeServerHTTP(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(str(Message("Server is running!")), "utf-8"))
 
     def do_POST(self):
-        
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(str(Message("Server is shutting down!")), "utf-8"))
         os.system("shutdown")
 
+    def do_PUT(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(str(Message("Server is restarting!")), "utf-8"))
+        os.system("shutdown -r")
+        
+
 
 def main():
-    
     server = HTTPServer((HOST, PORT), HomeServerHTTP)
     print("Server running...")
     server.serve_forever()
