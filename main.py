c1786965d717476d5822f373dca81e4df24575f7
Darkrider123 <cosmin.ariton@gmail.com>
2023-11-07 22:18:36 +0200

    added now keyword to shutdown command

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class HomeServerHTTP(BaseHTTPRequestHandler):
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(str(Message("Server is shutting down!")), "utf-8"))
-        os.system("shutdown")
+        os.system("shutdown now")
 
     def do_PUT(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(bytes(str(Message("Server is restarting!")), "utf-8"))
-        os.system("shutdown -r")
+        os.system("shutdown -r now")
         
 
 
